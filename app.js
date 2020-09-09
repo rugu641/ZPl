909a5a1c2aa0696b8f26e6b15b5ee3f560875b4f
amanjaiswal2507 <amanjaisw2507@gmail.com>
2020-09-09 17:39:27 +0530

    add home api

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const chalk = require('chalk');
-const morgan = require('morgan');
-const debug = require('debug')('app');
-const mysqlConnection = require('./connection');
-const bodyParser = require('body-parser');
-const playerRoutes = require('./routes/player');
+const express = require("express");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const debug = require("debug")("app");
+const mysqlConnection = require("./connection");
+const bodyParser = require("body-parser");
+const playerRoutes = require("./routes/player");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(bodyParser.json());
-app.use('/player', playerRoutes);
+app.use("/player", playerRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello from zpm");
 });
+app.get("/home", (req, res) => {
+    res.send("Hello AMan");
+});
 
-
-app.listen(5000, ()=>{
-    debug(`listening on port ${chalk.green(port)}`)
+app.listen(5000, () => {
+    debug(`listening on port ${chalk.green(port)}`);
 });
